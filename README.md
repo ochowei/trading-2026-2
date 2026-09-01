d43c6b8a18af651adee5caa6aeaed2c3517ad36a
William <ochowei@gmail.com>
2026-09-02 02:55:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# trading-2026-2
+
+Python 3.11 project managed with uv.
---
+++ tests/test_package.py
@@ -0,0 +1,4 @@
+def test_package_import() -> None:
+    import trading_2026_2
+
+    assert trading_2026_2.__version__ == "0.1.0"
